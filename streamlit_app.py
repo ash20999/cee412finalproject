11d01ced73ba50aeeea128839853f71e9b93c220
ash20999 <aarshpatel01012004@gmail.com>
2025-03-18 09:21:28 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+cee412p2_app/
+├─ streamlit_app.py         <-- Main file (Home page)
+├─ pages/
+│    ├─ 1_Introduction.py
+│    ├─ 2_Dataset.py
+│    ├─ 3_Analysis.py
+│    └─ 4_Conclusion.py
+├─ data/
+│    ├─ er_diagram.pdf      <-- (optional) for E/R diagram PDF
+│    └─ ...
+├─ requirements.txt         <-- (optional) recommended libraries and versions
+└─ README.md                <-- instructions for running the app
